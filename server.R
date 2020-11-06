949d6616bbaa3b88f8ec55b24689b4b71fe15567
TuQmano-Lenovo <juanpabloruiznicolini@gmail.com>
2020-11-06 19:48:19 -0300

    add draft server and ui files

--- server.R
+++ server.R
@@ -4,16 +4,16 @@ function(input, output) {
   
   # Filter data based on selections
   output$table <- DT::renderDataTable(DT::datatable({
-    data <- mpg
-    if (input$man != "All") {
-      data <- data[data$manufacturer == input$man,]
-    }
-    if (input$cyl != "All") {
-      data <- data[data$cyl == input$cyl,]
-    }
-    if (input$trans != "All") {
-      data <- data[data$trans == input$trans,]
-    }
+    data <- data.table::fread("data/turistas_internacionales_con destino.csv") 
+  #  if (input$man != "All") {
+  #    data <- data[data$manufacturer == input$man,]
+  #  }
+  #  if (input$cyl != "All") {
+  #    data <- data[data$cyl == input$cyl,]
+  #  }
+  #  if (input$trans != "All") {
+  #    data <- data[data$trans == input$trans,]
+  #  }
     data
   }))
   
