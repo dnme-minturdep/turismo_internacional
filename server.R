304d5b586f11da84397a611994539c7f85ca5124
Lei541 <lmabduca@yahoo.com>
2021-05-13 14:00:47 -0300

    Etiquetado en tablas de resultados

--- server.R
+++ server.R
@@ -4,96 +4,120 @@ function(input, output) {
   
   # TABLA RECEPTIVO
   output$table_receptivo <- DT::renderDataTable(
-
-      
-  DT::datatable(extensions = 'Buttons', options = list(
-    dom = 'Bfrtip',
-    buttons = 
-      list('copy', 'print', list(
-        extend = 'collection',
-        buttons = c('csv', 'excel', 'pdf'),
-        text = 'Download'
-      ))),    
-    {
-    
-    data_receptivo 
-    
-    if (input$year != "Todos") {
-      data_receptivo <- data_receptivo[data_receptivo$year %in% input$year,]		
-    }
-    if (input$mes != "Todos") {
-       data_receptivo <- data_receptivo[data_receptivo$mes %in% input$mes,]	
-    }  
-    if (input$paso_publ != "Todos") {
-      data_receptivo <- data_receptivo[data_receptivo$paso_publ == input$paso_publ,]
-    }
-    if (input$via != "Todas") {
-        data_receptivo <- data_receptivo[data_receptivo$via == input$via,]
-    }  
-    if (input$pais != "Todos") {
-        data_receptivo <- data_receptivo[data_receptivo$pais == input$pais,]
-    }
-    
-    if (input$paso_publ != "Todos") {
-        data_receptivo <- data_receptivo[data_receptivo$paso_publ == input$paso_publ,]
-    }
-    if (input$prov != "Todos") {
-        data_receptivo <- data_receptivo[data_receptivo$prov == input$prov,]
-    }
-    if (input$limita != "Todos") {
-        data_receptivo <- data_receptivo[data_receptivo$limita == input$limita,]
-    }
-    data_receptivo <- data_receptivo %>%
-        group_by_at(.vars = c(input$agrup)) %>%
-      summarise(round(sum(turistas))) 
     
     
-    data_receptivo
-  }))
+    DT::datatable(extensions = 'Buttons', options = list(
+      dom = 'Bfrtip',
+      buttons = 
+        list('copy', 'print', list(
+          extend = 'collection',
+          buttons = c('csv', 'excel', 'pdf'),
+          text = 'Download'
+        ))),    
+      {
+        
+        data_receptivo 
+        
+        if (input$year != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$year %in% input$year,]		
+        }
+        if (input$mes != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$mes %in% input$mes,]	
+        }  
+        if (input$paso_publ != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$paso_publ == input$paso_publ,]
+        }
+        if (input$via != "Todas") {
+          data_receptivo <- data_receptivo[data_receptivo$via == input$via,]
+        }  
+        if (input$pais != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$pais == input$pais,]
+        }
+        
+        if (input$paso_publ != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$paso_publ == input$paso_publ,]
+        }
+        if (input$prov != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$prov == input$prov,]
+        }
+        if (input$limita != "Todos") {
+          data_receptivo <- data_receptivo[data_receptivo$limita == input$limita,]
+        }
+        data_receptivo <- data_receptivo %>%
+          group_by_at(.vars = c(input$agrup)) %>%
+          summarise (Turistas = round(sum(turistas))) 
+       
+        #etiquetas receptivo según selección en ui.
+        
+        etiquetas <- gsub ("year", "Año", (colnames(data_receptivo)))
+        etiquetas <- gsub ("mes", "Mes", etiquetas)
+        etiquetas <- gsub ("via", "Vía", etiquetas)
+        etiquetas <- gsub ("pais" , "País de residencia", etiquetas)
+        etiquetas <- gsub ("paso_publ" , "Paso", etiquetas)
+        etiquetas <- gsub ("prov", "Provincia del paso", etiquetas)
+        etiquetas <- gsub ("limita" ,"Limita con", etiquetas)
+        
+        data_receptivo
+      }, rownames= FALSE, colnames = etiquetas)
+    )
   
- 
+  
   
   
   
   # TABLA EMISIVO
   output$table_emisivo <- DT::renderDataTable(
     
-  DT::datatable(
-    {
-    
-    data_emisivo 
-    
-      if (input$year_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$year %in% input$year_e,]		
-      }
-      if (input$mes_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$mes %in% input$mes_e,]	
-      }  
-      if (input$paso_publ_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$paso_publ == input$paso_publ_e,]
-      }
-      if (input$via_e != "Todas") {
-        data_emisivo <- data_emisivo[data_emisivo$via == input$via_e,]
-      }  
-      if (input$destino != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$destino_agrup == input$destino,]
-      }
-      
-      if (input$paso_publ_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$paso_publ == input$paso_publ_e,]
-      }
-      if (input$prov_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$prov == input$prov_e,]
-      }
-      if (input$limita_e != "Todos") {
-        data_emisivo <- data_emisivo[data_emisivo$limita == input$limita_e,]
-      }
-      data_emisivo <- data_emisivo %>%
-        group_by_at(.vars = c(input$agrup_e)) %>%
-        summarise(round(sum(turistas))) 
-      
-    data_emisivo
-  }))
+    DT::datatable(
+      {
+        
+        data_emisivo 
+        
+        if (input$year_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$year %in% input$year_e,]		
+        }
+        if (input$mes_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$mes %in% input$mes_e,]	
+        }  
+        if (input$paso_publ_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$paso_publ == input$paso_publ_e,]
+        }
+        if (input$via_e != "Todas") {
+          data_emisivo <- data_emisivo[data_emisivo$via == input$via_e,]
+        }  
+        if (input$destino != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$destino_agrup == input$destino,]
+        }
+        
+        if (input$paso_publ_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$paso_publ == input$paso_publ_e,]
+        }
+        if (input$prov_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$prov == input$prov_e,]
+        }
+        if (input$limita_e != "Todos") {
+          data_emisivo <- data_emisivo[data_emisivo$limita == input$limita_e,]
+        }
+        data_emisivo <- data_emisivo %>%
+          group_by_at(.vars = c(input$agrup_e)) %>%
+          summarise(Turistas = round(sum(turistas))) 
+        
+        #etiquetas emisivo según selección en ui.
+        
+        etiquetas_e <- gsub ("year", "Año", (colnames(data_emisivo)))
+        etiquetas_e <- gsub ("mes", "Mes", etiquetas_e)
+        etiquetas_e <- gsub ("via", "Vía", etiquetas_e)
+        etiquetas_e <- gsub ("destino_agrup" , "Destino principal", etiquetas_e)
+        etiquetas_e <- gsub ("paso_publ" , "Paso", etiquetas_e)
+        etiquetas_e <- gsub ("prov", "Provincia del paso", etiquetas_e)
+        etiquetas_e <- gsub ("limita" ,"Limita con", etiquetas_e)
+        
+        data_emisivo
+      }, rownames= FALSE, colnames = etiquetas_e)
+       )
   
   
 }
+        
+        
+        
